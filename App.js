f84a47c60639ee7e517ce5f649a87d914a16eb25
Steven <stevenklavins94@gmail.com>
2020-07-14 17:13:01 +0100

    feat: added opening screen

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, setState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import Game from './components/Game'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isGame, setIsGame] = useState(false)
+
+  const enterGame = () => {
+    setIsGame(true)
+  }
+
+  if (isGame === false) {
+    return (
+      <View style={styles.container}>
+        <Text>Bug Thug</Text>
+        <StatusBar style="auto" />
+        <View style={styles.startButton}>
+          <Button title="Start" onPress={enterGame} />
+        </View>
+      </View>
+    )
+  } else {
+    return <Game />
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  startButton: {
+    width: '40%',
+    padding: 20,
+    color: 'black',
+  },
+})

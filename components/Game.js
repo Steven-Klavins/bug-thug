9fe6804dabea3bdc836e14897806abb3844c1e20
Graham <grahamfalconer@icloud.com>
2020-07-20 16:31:03 +0100

    changed game to react component

--- components/Game.js
+++ components/Game.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
 
-export default function Game() {
-  return (
-    <View>
-      <Text>Here is the game!</Text>
-    </View>
-  )
-}
+export default class Game extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text> Hi There, this is my game </Text>
+      </View>
+    )
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+    flex: 1,
+  }
 })
+
+
+
+
+
+// import React from 'react'
+// import { StyleSheet, Text, View, Button } from 'react-native'
+
+// export default function Game() {
+//   return (
+//     <View>
+//       <Text>Here is the game!</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// })
